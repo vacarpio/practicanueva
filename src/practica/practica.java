ae580e5037738e1aa0c2389ee12b5ea0a174880e
ACER <ACER@VickyCarpio>
2019-12-20 07:43:50 -0500

    Aumente una linea nueva

---
+++ src/practica/practica.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica;
+
+/**
+ *
+ * @author ACER
+ */
+public class practica {
+     public static void main(String[] args) {
+       practicaventana v=new practicaventana();
+       v.setVisible(true);
+       v.setLocationRelativeTo(null);
+        
+    }
+}
